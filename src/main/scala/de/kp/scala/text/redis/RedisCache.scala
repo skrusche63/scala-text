2a911e2bf749b7263fa58173676df2f06421e937
skrusche63 <skrusche63@googlemail.com>
2014-11-14 16:40:41 +0100

    Caching support extended.

--- src/main/scala/de/kp/scala/text/redis/RedisCache.scala
+++ src/main/scala/de/kp/scala/text/redis/RedisCache.scala
@@ -46,6 +46,18 @@ object RedisCache {
     client.zadd(k,timestamp,v)
     
   }
+  
+  def addRequest(req:ServiceRequest) {
+    
+    val now = new Date()
+    val timestamp = now.getTime()
+    
+    val k = "request:" + service
+    val v = "" + timestamp + ":" + Serializer.serializeRequest(req)
+    
+    client.lpush(k,v)
+    
+  }
 
   def addTopics(req:ServiceRequest,topics:String) {
     addTopics(req.data("uid"),topics)
@@ -77,54 +89,83 @@ object RedisCache {
     
   }
   
-  /**
-   * Get timestamp when job with 'uid' started
-   */
-  def starttime(uid:String):Long = {
-    
-    val k = "job:" + service + ":" + uid
-    val jobs = client.zrange(k, 0, -1)
+  def requestsTotal():Long = {
 
-    if (jobs.size() == 0) {
-      0
+    val k = "request:" + service
+    if (client.exists(k)) client.llen(k) else 0
     
-    } else {
+  }
+  
+  def requests(start:Long,end:Long):List[(Long,ServiceRequest)] = {
+    
+    val k = "request:" + service
+    val requests = client.lrange(k, start, end)
+    
+    requests.map(request => {
       
-      val first = jobs.iterator().next()
-      first.split(":")(0).toLong
+      val Array(ts,req) = request.split(":")
+      (ts.toLong,Serializer.deserializeRequest(req))
       
-    }
-     
+    }).toList
+    
   }
   
   def status(uid:String):String = {
 
     val k = "job:" + service + ":" + uid
-    val jobs = client.zrange(k, 0, -1)
+    val data = client.zrange(k, 0, -1)
 
-    if (jobs.size() == 0) {
+    if (data.size() == 0) {
       null
     
     } else {
       
-      val job = Serializer.deserializeJob(jobs.toList.last)
+      /* Format: timestamp:jobdesc */
+      val last = data.toList.last
+      val Array(timestamp,jobdesc) = last.split(":")
+      
+      val job = Serializer.deserializeJob(jobdesc)
       job.status
       
     }
 
   }
   
-  def topics(uid:String):String = {
+  def statuses(uid:String):List[(Long,JobDesc)] = {
+    
+    val k = "job:" + service + ":" + uid
+    val data = client.zrange(k, 0, -1)
 
-    val k = "topics:" + service + ":" + uid
-    val topics = client.zrange(k, 0, -1)
-
-    if (topics.size() == 0) {
+    if (data.size() == 0) {
       null
     
     } else {
       
-      topics.toList.last
+      data.map(record => {
+        
+        val Array(timestamp,jobdesc) = record.split(":")
+        (timestamp.toLong,Serializer.deserializeJob(jobdesc))
+        
+      }).toList
+      
+    }
+    
+  }  
+  
+  def topics(uid:String):String = {
+
+    val k = "topics:" + service + ":" + uid
+    val data = client.zrange(k, 0, -1)
+
+    if (data.size() == 0) {
+      null
+    
+    } else {
+      
+      val last = data.toList.last
+      val Array(timestamp,topic) = last.split(":")
+      
+      topic
       
     }
 
